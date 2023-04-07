0e009d40f05ad7492b8affcb3ff9577dec2f9e0b
Kharlamov <tarantoga90@gmail.com>
2023-04-07 19:24:25 +0400

    task completed

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String fileContent = "";
+        Profile profile = new Profile();
+
+        // IO Input Stream from previous task
+        try (BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(file))) {
+            fileContent = new String(fileInputStream.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Using Files class from nio package
+        try {
+           fileContent = Files.readString(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        if (!fileContent.isEmpty()) {
+            String[] keyValueArray = fileContent.split("\n");
+
+            if (keyValueArray.length == 4) {
+                profile.setName(keyValueArray[0].substring(keyValueArray[0].indexOf(" ")).trim());
+                profile.setAge(Integer.parseInt(keyValueArray[1].substring(keyValueArray[1].indexOf(" ")).trim()));
+                profile.setEmail(keyValueArray[2].substring(keyValueArray[2].indexOf(" ")).trim());
+                profile.setPhone(Long.parseLong(keyValueArray[3].substring(keyValueArray[3].indexOf(" ")).trim()));
+            }
+        }
+
+        return profile;
     }
 }
